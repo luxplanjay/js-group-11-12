503e0d98b41a21b4114a7d0ea375363d0aaa759f
Alexander Repeta <axzerk@gmail.com>
2019-03-19 21:46:01 +0200

    Adds examples

---
+++ src/app.js
@@ -0,0 +1,119 @@
+'use strict';
+
+// const value = 20.5;
+
+// const name = 'Mango';
+
+// let isActive = false;
+
+// const hasChildNodes = false;
+
+// const type = typeof isActive;
+
+// ===============================
+
+// const isGoing = confirm('Вы точно летите отдыхать?');
+
+// console.log(isGoing);
+
+// ===============================
+
+// const input = prompt('Введите ваше имя!');
+
+// console.log(typeof input);
+
+// const min = 10;
+// const max = 30;
+// const number = 45;
+
+// const isNumberInRange = number > min && number < max;
+
+// console.log(isNumberInRange);
+
+// ===============================
+
+// let input = prompt('Угадай число от 1 до 10'); // '5'
+
+// console.log('input перед преобразованием: ', input);
+
+// input = Number(input);
+
+// const isNaN = Number.isNaN(input);
+
+// console.log('input после преобразования: ', input);
+// console.log('isNaN?: ', isNaN);
+
+// const isCorrect = input >= 1 && input <= 10;
+
+// =======================================
+// console.log(isCorrect);
+
+// const boxWidthInPx = '25.5px';
+
+// const width = Number.parseFloat(boxWidthInPx);
+
+// console.log(width);
+
+// console.log((0.2 * 100 + 0.1 * 100) / 100);
+
+// =======================================
+
+// const number = Math.floor(2.6);
+
+// console.log(number);
+
+// =======================================
+
+const firstName = 'Jay';
+const lastName = 'Lux';
+const fullName = `${firstName} ${lastName}`;
+
+console.log(fullName);
+
+const name = 'Mango';
+const age = 2;
+const mood = 'happy';
+
+// My name is [name], I'm [age] years old and [mood].
+
+// const message =
+//   'My name is ' + name + ", I'm " + age + ' years old and ' + mood + '.';
+
+const message = `My name is ${name}, I'm ${age} years old and ${mood}.`;
+
+console.log(message);
+
+// =======================================
+
+// const productTitle = 'Samsung is cool';
+// const searchString = 'samS';
+
+// const inLowerCase = title.toLowerCase();
+// const inUpperCase = title.toUpperCase();
+
+// console.log(inLowerCase);
+// console.log(inUpperCase);
+// console.log(title);
+
+// const titleInLC = productTitle.toLowerCase();
+// const searchStringInLC = searchString.toLowerCase();
+
+// const passed = titleInLC.includes(searchStringInLC);
+
+// console.log(passed);
+
+// console.log();
+
+// console.log(title.includes('Java'));
+
+// ========
+
+const date = 10;
+const month = 3;
+const year = 1800;
+
+console.log(`${date}\\${month}\\${year}`);
+
+if (userChoice !== null && userChoice !== '') {
+  alert;
+}
