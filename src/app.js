7a2835eab4fe199b8b5b529c92ccc1f20a8327ae
Alexander Repeta <axzerk@gmail.com>
2019-03-26 20:56:14 +0200

    Adds examples

---
+++ src/app.js
@@ -0,0 +1,63 @@
+'use strict';
+
+// const message = 'Lorem ipsums dolor sit';
+// console.log(message);
+
+// const words = message.split(' ');
+// console.log(words);
+
+// const snakeCaseMessage = words.join('_');
+// console.log(snakeCaseMessage);
+
+// ================================
+// const products = ['apples', 'grapes', 'bananas'];
+
+// const indexOfApples = products.indexOf('apples');
+// const isInProducts = products.includes('apples123');
+
+// console.log(isInProducts);
+
+// ==============================
+// const users = ['Mango', 'Ajax', 'Poly'];
+// const login = prompt('Введите логин!');
+
+// const isRegistered = users.includes(login);
+
+// if (isRegistered) {
+//   console.log('Добро пожаловать!');
+// } else {
+//   console.log('Неверный логин!');
+// }
+
+// ==============================
+// а можно как то  допустим 5 карточку перенести ее на место второй? как перетягивание карточек трело
+/*
+  1. найти пятую карточку ее индекс
+  2. Вырезать элемент с таким индексом и сохранить
+  3. Вставить ее
+*/
+// const cards = ['card-1', 'card-2', 'card-3', 'card-5', 'card-4'];
+
+// const index = cards.indexOf('card-5');
+// const card = cards.splice(index, 1)[0];
+
+// cards.splice(1, 0, card);
+
+// console.log(cards);
+
+// ==============================
+
+// const images = ['image-1.jpg', 'image-2.jpg', 'image-3.jpg'];
+
+// let currentImageIdx = 0;
+// console.log(images[currentImageIdx]);
+
+// currentImageIdx += 1;
+// console.log(images[currentImageIdx]);
+
+// currentImageIdx += 1;
+// console.log(images[currentImageIdx]);
+
+// currentImageIdx -= 1;
+// console.log(images[currentImageIdx]);
+
