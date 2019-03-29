701bcd863f71bd6b552a94c4561c6e2f0774d483
Alexander Repeta <axzerk@gmail.com>
2019-03-29 21:09:40 +0200

    Adds examples

--- src/app.js
+++ src/app.js
@@ -1,63 +1,162 @@
 'use strict';
 
-// const message = 'Lorem ipsums dolor sit';
-// console.log(message);
+// let counter = 0;
 
-// const words = message.split(' ');
-// console.log(words);
+// while (counter < 5) {
+//   console.log(counter);
 
-// const snakeCaseMessage = words.join('_');
-// console.log(snakeCaseMessage);
-
-// ================================
-// const products = ['apples', 'grapes', 'bananas'];
-
-// const indexOfApples = products.indexOf('apples');
-// const isInProducts = products.includes('apples123');
-
-// console.log(isInProducts);
-
-// ==============================
-// const users = ['Mango', 'Ajax', 'Poly'];
-// const login = prompt('Введите логин!');
-
-// const isRegistered = users.includes(login);
-
-// if (isRegistered) {
-//   console.log('Добро пожаловать!');
-// } else {
-//   console.log('Неверный логин!');
+//   counter += 1;
 // }
 
-// ==============================
-// а можно как то  допустим 5 карточку перенести ее на место второй? как перетягивание карточек трело
+// let userInput;
+
+// do {
+//   userInput = prompt('давай вводи число 10!');
+//   console.log(userInput);
+//   // Выполняем до тех пор пока не равно null и не равно 10
+// } while (userInput !== null && Number(userInput) !== 10);
+
+// Пишем условие ВЫПОЛНЕНИЯ
+// userInput = null
+// null !== null || Number(null) !== 10
+// false || true -> true
+
+// userInput = 10
+// 10 !== null || Number(10) !== 10
+// true || false -> true
+
+// userInput = null
+// null !== null && Number(null) !== 10
+// false && true -> false
+
+// userInput = 10
+// 10 !== null && Number(10) !== 10
+// true && false -> false
+
+// ==================
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   // console.log('i: ', i);
+//   // console.log(`numbers[${i}]:`, numbers[i]);
+
+//   numbers[i] = numbers[i] * 2;
+// }
+
+// console.log(numbers);
+
+// ================================
+
+// const matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+// for (let i = 0; i < matrix.length; i += 1) {
+//   console.group(`Iteration ${i}`);
+
+//   console.log('i: ', i);
+//   console.log(`matrix[${i}]: `, matrix[i]);
+
+//   for (let j = 0; j < matrix[i].length; j += 1) {
+//     console.log('j: ', j);
+//     console.log(`matrix[${i}][${j}]: `, matrix[i][j]);
+//   }
+
+//   console.groupEnd(`Iteration ${i}`);
+// }
+
+// ================================
+
 /*
-  1. найти пятую карточку ее индекс
-  2. Вырезать элемент с таким индексом и сохранить
-  3. Вставить ее
-*/
-// const cards = ['card-1', 'card-2', 'card-3', 'card-5', 'card-4'];
+ * Напиши скрипт, который проверяет произвольную строку
+ * в переменной message и находит в ней самое длинное слово,
+ * записывая его в переменную longestWord.
+ */
 
-// const index = cards.indexOf('card-5');
-// const card = cards.splice(index, 1)[0];
+// 1. Сделать из строки массив
+// 2. Перебрать массив в цикле
+// Переменная для хранения самого длинного слова и делаем предположение что самое длинное слово это первое
+// на каждой итерации сравнить текущее и новое
+// если новое больше текущего то перезаписать
 
-// cards.splice(1, 0, card);
+// const message = 'May the force be with you';
+// const words = message.split(' ');
+// let longestWord = words[0];
 
-// console.log(cards);
+// for (let i = 1; i < words.length; i += 1) {
+//   const currentWord = words[i];
+//   const currentWordLength = currentWord.length;
+//   const longestWordLength = longestWord.length;
 
-// ==============================
+//   if (currentWordLength > longestWordLength) {
+//     longestWord = currentWord;
+//   }
+// }
 
-// const images = ['image-1.jpg', 'image-2.jpg', 'image-3.jpg'];
+// for (const word of words) {
+//   if (word.length > longestWord.length) {
+//     longestWord = word;
+//   }
+// }
 
-// let currentImageIdx = 0;
-// console.log(images[currentImageIdx]);
+// console.log(words);
+// console.log(longestWord); // 'force'
 
-// currentImageIdx += 1;
-// console.log(images[currentImageIdx]);
+// ===========================
+// const message = 'qq www rrr eeee yy ttttt ';
+// const words = message.split(' ');
+// const max = words.length - 1;
+// const leftCount = 4;
+// const rightCount = 5;
+// let target;
 
-// currentImageIdx += 1;
-// console.log(images[currentImageIdx]);
+// for (let i = 1; i < max; i += 1) {
+//   const prevElLength = words[i - 1].length;
+//   const nextElLength = words[i + 1].length;
 
-// currentImageIdx -= 1;
-// console.log(images[currentImageIdx]);
+//   if (prevElLength === leftCount && nextElLength === rightCount) {
+//     target = words[i];
+//   }
+// }
+
+// console.log(words);
+// console.log(target);
+// =========================================
+
+// const clients = ['Mango', 'Poly', 'Ajax'];
+// const clientNameToFind = 'Poly';
+// let message;
+
+// for (let i = 0; i < clients.length; i += 1) {
+//   console.log('i: ', i);
+//   console.log('clients[i]: ', clients[i]);
+//   console.log('clientNameToFind: ', clientNameToFind);
+
+//   if (clients[i] === clientNameToFind) {
+//     message = 'Найден :)';
+//     break;
+//   }
+
+//   message = 'Не найден :(';
+// }
+
+// console.log(message);
+
+// =========================================
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// // console.log(numbers.includes(3));
+
+// const numberToFind = 13;
+
+// let result = false;
+
+// for (const number of numbers) {
+//   if (number === numberToFind) {
+//     result = true;
+//     break;
+//   }
+// }
+
+// console.log(result);
 
